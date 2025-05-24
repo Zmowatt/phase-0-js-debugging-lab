c2c60623df253dc4e56c5c160c1276147e74f083
Zachary Mowatt <zamowatt@gmail.com>
2025-05-23 20:59:58 -0400

    Completing phase 0 js debugging lab

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,18 @@
+
+debugger
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+    debugger
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
-
+debugger
     return totalPrice;
 }
-
 
 
 module.exports = calculateDiscountedPrice;
